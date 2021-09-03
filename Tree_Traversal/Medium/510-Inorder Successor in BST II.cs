3738494fb33c636b02119dbcd95761d07d050a98
Garga124 <gargaruhi9@gmail.com>
2021-09-03 22:53:16 +0100

    Create 510-Inorder Successor in BST II.cs

---
+++ Tree_Traversal/Medium/510-Inorder Successor in BST II.cs
@@ -0,0 +1,23 @@
+/*
+Time Complexity : O(H)
+Space Complexity : O(1)
+// Definition for a Node.
+public class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node parent;
+}
+*/
+
+public class Solution {
+    public Node InorderSuccessor(Node x) {
+        if (x.right != null) {
+          x = x.right;
+          while (x.left != null) x = x.left;
+          return x;
+        }
+        while (x.parent != null && x == x.parent.right) x = x.parent;
+        return x.parent;
+    }
+}
